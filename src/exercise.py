1a73b5d8b13a17cce567c3bf483de34b09449a77
MrSullivan <>
2021-05-16 21:35:22 +0000

    code

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    division(21, 5)
+
+
+def division(numerator, denominator):
+    
+    value = float(numerator / denominator)
+
+    print(value)
 
 if __name__ == '__main__':
     main()
